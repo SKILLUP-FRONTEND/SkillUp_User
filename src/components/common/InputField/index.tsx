67c4de0280b60025f1580500b4e2566692c30fdf
gracekim527 <k07173027@gmail.com>
2025-11-01 12:14:13 +0900

    feat: InputField 컴포넌트에 description 옵션 추가

--- src/components/common/InputField/index.tsx
+++ src/components/common/InputField/index.tsx
@@ -5,15 +5,28 @@ import Text from "../Text";
 
 interface InputFieldProps {
   label: string;
+  description?: string;
   children: React.ReactNode;
 }
 
-export default function InputField({ label, children }: InputFieldProps) {
+export default function InputField({
+  label,
+  description,
+  children,
+}: InputFieldProps) {
   return (
     <div className={styles.inputField}>
-      <Text typography="label3_m_14" color="black" as="span">
-        {label}
-      </Text>
+      <div className={styles.inputFieldLabel}>
+        <Text typography="label3_m_14" color="black" as="span">
+          {label}
+        </Text>
+        {description && (
+          <Text typography="label4_m_12" color="neutral-70" as="span">
+            {description}
+          </Text>
+        )}
+      </div>
+
       {children}
     </div>
   );
