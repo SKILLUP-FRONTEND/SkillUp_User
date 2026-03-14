6d648c09716d905bb48068529557105b99e44a9d
gracekim527 <k07173027@gmail.com>
2026-03-14 14:40:46 +0900

    fix: keep users on home after onboarding

--- src/components/common/OnboardingModalController/index.tsx
+++ src/components/common/OnboardingModalController/index.tsx
@@ -24,7 +24,6 @@ export default function OnboardingModalController() {
 
   const handleSaved = () => {
     setShowOnboarding(false);
-    router.push("/profile/edit");
   };
 
   const handleRejoin = () => {
